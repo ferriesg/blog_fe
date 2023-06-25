0dd0bac218165e8d55c01dd1422f80d586c087e6
weiming <ferries_g@163.com>
2023-06-25 15:05:35 +0800

    add new tempalte

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ function Home(props: any) {
               </BlogItem>
             );
           })} */}
-          test github
+          test github1
         </Box>
         <Box flex={1}>
           <CardItem height="100px"></CardItem>
